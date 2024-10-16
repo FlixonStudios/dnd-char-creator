947fe54e72f5d0f84bf2ec70ca1044f6c3aa6d41
FlixonStudios <chiakaiming@gmail.com>
2024-10-16 14:47:51 +0800

    add footer content

--- src/components/general/Footer.js
+++ src/components/general/Footer.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
 
-export function Footer(props) {
-    let style = {
-        backgroundColor:`rgba(0,0,0,0.7)`,
-        height:"10px",
-        left:"0",
-        bottom:"0",
-        position:"fixed",
-    }
+export function Footer({ isMock }) {
+  let style = {
+    backgroundColor: `rgba(255,0,0,0.7)`,
+    left: "0",
+    bottom: "0",
+    position: "fixed",
+    width: "100%",
+  };
 
-
+  function renderDisclaimer() {
+    let disclaimerStyle = {
+      backgroundColor: `rgba(0,0,0,0.7)`,
+      padding: `8px`,
+      display: `flex`,
+      flexDirection: `column`,
+      alignItems: `center`,
+      justifyContent: `center`,
+    };
     return (
-        <div style={style}>
-
-        </div>
+      <div style={disclaimerStyle}>
+        <p style={{ color: "white" }}>
+          Disclaimer: This site is a not commercial and is not official. Just my
+          personal project.
+        </p>
+      </div>
     );
+  }
+
+  return <div style={style}>{renderDisclaimer()}</div>;
 }
 
 //export default Footer;
