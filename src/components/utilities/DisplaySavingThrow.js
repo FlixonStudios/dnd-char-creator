65509f42489b7992b0a0eb5b6129cbf26c36aa6e
FlixonStudios <chiakaiming@gmail.com>
2024-10-17 13:35:22 +0800

    clean up saving throws

--- src/components/utilities/DisplaySavingThrow.js
+++ src/components/utilities/DisplaySavingThrow.js
@@ -1,66 +1,52 @@
-import React from 'react';
-import {Col, Row} from "react-bootstrap";
+import React from "react";
+import { Col, Row } from "react-bootstrap";
 
-export function DisplaySavingThrow({character}) {
+export function DisplaySavingThrow({ character }) {
+  let throwModifiers = character["saving-throws"];
+  let STR = "str";
+  let DEX = "dex";
+  let CON = "con";
+  let INT = "int";
+  let WIS = "wis";
+  let CHA = "cha";
 
-    let throwModifiers = character['saving-throws']
-    let STR = 'str';let DEX = 'dex';let CON = 'con';let INT = 'int';let WIS = 'wis';let CHA = 'cha'
+  function renderModifiers(statArr) {
     return (
-        <div>
-            <Row>
-                <Col md={3}>
-                    <h6 className={"text-white text-center"}>STR</h6>
-                </Col>
-                <Col md={1}>
-                    <h6 className={"text-white text-center"}>
-                        {throwModifiers[STR]>0?`+${throwModifiers[STR]}`:throwModifiers[STR]}
-                    </h6>
-                </Col>
-                <Col md={3}>
-                    <h6 className={"text-white text-center"}>DEX</h6>
-                </Col>
-                <Col md={1}>
-                    <h6 className={"text-white text-center"}>
-                        {throwModifiers[DEX]>0?`+${throwModifiers[DEX]}`:throwModifiers[DEX]}
-                    </h6>
+      <>
+        {statArr.map((stat) => {
+          return (
+            <Col md={4} key={stat}>
+              <Row md={12}>
+                <Col md={8}>
+                  <h6 className={"text-white text-center"}>
+                    {stat.toUpperCase()}
+                  </h6>
                 </Col>
                 <Col md={3}>
-                    <h6 className={"text-white text-center"}>CON</h6>
+                  <h6 className={"text-white text-center"}>
+                    {throwModifiers[stat] > 0
+                      ? `+${throwModifiers[stat]}`
+                      : throwModifiers[stat]}
+                  </h6>
                 </Col>
-                <Col md={1}>
-                    <h6 className={"text-white text-center"}>
-                        {throwModifiers[CON]>0?`+${throwModifiers[CON]}`:throwModifiers[CON]}
-                    </h6>
-                </Col>
-            </Row>
-            <Row>
-                <Col md={3}>
-                    <h6 className={"text-white text-center"}>INT</h6>
-                </Col>
-                <Col md={1}>
-                    <h6 className={"text-white text-center"}>
-                        {throwModifiers[INT]>0?`+${throwModifiers[INT]}`:throwModifiers[INT]}
-                    </h6>
-                </Col>
-                <Col md={3}>
-                    <h6 className={"text-white text-center"}>WIS</h6>
-                </Col>
-                <Col md={1}>
-                    <h6 className={"text-white text-center"}>
-                        {throwModifiers[WIS]>0?`+${throwModifiers[WIS]}`:throwModifiers[WIS]}
-                    </h6>
-                </Col>
-                <Col md={3}>
-                    <h6 className={"text-white text-center"}>CHA</h6>
-                </Col>
-                <Col md={1}>
-                    <h6 className={"text-white text-center"}>
-                        {throwModifiers[CHA]>0?`+${throwModifiers[CHA]}`:throwModifiers[CHA]}
-                    </h6>
-                </Col>
-            </Row>
-        </div>
+              </Row>
+            </Col>
+          );
+        })}
+      </>
     );
+  }
+
+  function renderRow(statArr) {
+    return <Row>{renderModifiers(statArr)}</Row>;
+  }
+
+  return (
+    <div>
+      {renderRow([STR, DEX, CON])}
+      {renderRow([INT, WIS, CHA])}
+    </div>
+  );
 }
 
 export default DisplaySavingThrow;
